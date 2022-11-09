678a9f8d4fa992d3ccb9802499a8748727d6954f
zero0722 <r07zero@gmail.com>
2022-11-09 14:36:55 +0900

    git課題4

---
+++ 20221102/TaskA.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+
+int main(){
+    //printf("Hello World");
+    for(int i=0;i<=10;i++){
+        printf("%d\n",i) ;
+    }
+
+    return 0;
+}
---
+++ 20221102/TaskB.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<time.h>
+
+int main(void){
+    int i;
+    int rand_num[10];
+    srand(time(NULL));
+    
+    for(i = 0;i < 10;i++){
+        rand_num[i] = rand();
+        printf("%d\n",rand_num[i]);
+    }
+
+    return 0;
+}
